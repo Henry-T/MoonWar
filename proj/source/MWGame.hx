8f78d19b06c47533ddbc33563cc4876beb04e2b8
henryt <tang.hy@live.com>
2013-04-28 08:21:05 +0800

    自定义暂停 把FlxGame.hx- createFocusScreen函数里面不许要的logo、箭头注释掉了 加上一行paused ...文字

--- proj/source/MWGame.hx
+++ proj/source/MWGame.hx
@@ -5,28 +5,28 @@ import org.flixel.FlxGame;
 
 class MWGame extends FlxGame
 {
-public function new()
-{
-	FlxG.debug = true;
-	FlxG.visualDebug = true;
-	
-	super(550,400,MainMenu,1);
-	//super(550,400,IntroScreen,1);
-	//super(550, 400,Tutorial,1);
-	//super(550, 400,Level2,1);
-	//super(550, 400, Level3, 1);
-	//super(550, 400, Level4, 1);
-	//super(550, 400, Level5, 1);
-	//super(550, 400, Level6, 1);
-	//super(550, 400, Level7, 1);
-	
-	//super(550, 400, Win, 1);
-	//super(550, 400, GameOver, 1);
-	
-	//super(550,400,TestBed);
-	
-	FlxG.framerate = 30;
-	FlxG.flashFramerate = 30;
-	GameStatic.Load();
-}
+	public function new()
+	{
+		FlxG.debug = true;
+		FlxG.visualDebug = true;
+		
+		super(550,400,MainMenu,1);
+		//super(550,400,IntroScreen,1);
+		//super(550, 400,Tutorial,1);
+		//super(550, 400,Level2,1);
+		//super(550, 400, Level3, 1);
+		//super(550, 400, Level4, 1);
+		//super(550, 400, Level5, 1);
+		//super(550, 400, Level6, 1);
+		//super(550, 400, Level7, 1);
+		
+		//super(550, 400, Win, 1);
+		//super(550, 400, GameOver, 1);
+		
+		//super(550,400,TestBed);
+		
+		FlxG.framerate = 30;
+		FlxG.flashFramerate = 30;
+		GameStatic.Load();
+	}
 }
