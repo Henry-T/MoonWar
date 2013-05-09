671fcabb4480ae3dda635382d5c027537b806a01
henryt <tang.hy@live.com>
2013-05-09 19:43:16 +0800

    将按钮图片路径和pixelex字体路径加入ResUtil

--- proj/source/ResUtil.hx
+++ proj/source/ResUtil.hx
@@ -5,6 +5,13 @@ import org.flixel.FlxG;
 
 class ResUtil 
 {
+	public static var FNT_Pixelex:String = "assets/fnt/pixelex.ttf";
+
+	public static var IMG_ui_box_yellow = "assets/img/ui_box_y.png";
+	public static var IMG_ui_box_act_yellow = "assets/img/ui_boxact_y.png";
+	public static var IMG_ui_box_blue = "assets/img/ui_box_b.png";
+	public static var IMG_ui_box_act_blue = "assets/img/ui_boxact_b.png";
+
 	public static function playTitle() : Void
 	{
 		FlxG.playMusic("title");
