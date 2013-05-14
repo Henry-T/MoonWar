9f9202f3eca61a06eeffbd01f6fbf54bfa81f64c
henryt <tang.hy@live.com>
2013-05-14 12:21:08 +0800

    mod 同上

--- proj/source/Level1.hx
+++ proj/source/Level1.hx
@@ -94,7 +94,6 @@ class Level1 extends Level
 	override public function update():Void
 	{
 		super.update();
-		if(confirmReady) return;
 		if(confirmReady || FlxG.paused || endPause)	return;
 
 		FlxG.overlap(bot, end, function(b:FlxObject, e:FlxObject) { 
